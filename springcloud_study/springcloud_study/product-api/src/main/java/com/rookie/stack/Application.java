a5fd884a3172ce2014be3abd111459c943f65a78
ashbur <18829526908@163.com>
2024-06-26 21:13:42 +0800

    做部分修改

--- springcloud_study/springcloud_study/product-api/src/main/java/com/rookie/stack/Application.java
+++ springcloud_study/springcloud_study/product-api/src/main/java/com/rookie/stack/Application.java
@@ -2,6 +2,7 @@ package com.rookie.stack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author eumenides
