4dbf12f59a1f54c76cdf20838cd97ef9ecbbe3e3
ashbur <18829526908@163.com>
2024-06-26 08:27:32 +0800

    vue3进阶

---
+++ vue3-study/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ person.name }}</h2>
+    <h2>年龄：{{ person.age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="changePerson">修改整个人</button>
+  </div>
+</template>
+
+<script lang="ts" setup name="Person">
+  import {ref,watch} from 'vue'
+  // 数据
+  let person = ref({
+    name:'张三',
+    age:18
+  })
+  // 方法
+  function changeName(){
+    person.value.name += '~'
+  }
+  function changeAge(){
+    person.value.age += 1
+  }
+  function changePerson(){
+    person.value = {name:'李四',age:90}
+  }
+  watch(person,(newValue,oldValue)=>{
+    console.log('person变化了',newValue,oldValue)
+  })
+</script>
+
+<style scoped>
+
+</style>
