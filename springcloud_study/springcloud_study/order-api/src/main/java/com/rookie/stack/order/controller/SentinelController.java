2988c17f0fb0a3c10e6c93ea77b96e843bcc8122
ashbur <18829526908@163.com>
2024-06-27 21:31:23 +0800

    一个小的 react demo

--- springcloud_study/springcloud_study/order-api/src/main/java/com/rookie/stack/order/controller/SentinelController.java
+++ springcloud_study/springcloud_study/order-api/src/main/java/com/rookie/stack/order/controller/SentinelController.java
@@ -1,5 +1,6 @@
 package com.rookie.stack.order.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,9 @@ import java.util.concurrent.TimeUnit;
  * @date 2024/6/27
  */
 @RestController
+@Slf4j
 public class SentinelController {
+
 
     @GetMapping("/sentinel")
     public String sentinel(){
@@ -25,6 +28,7 @@ public class SentinelController {
 
     @GetMapping("/sentinel2")
     public String sentinel2(){
+        log.info("sleuth ....");
         return "sentinel";
     }
 
