1bd4d67ab8ee0f6f08d24cba4f50ac027d2341c5
ashbur <18829526908@163.com>
2024-06-29 17:34:32 +0800

    我也不知道我改了啥

---
+++ css_study/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>新拟态</div>
+</template>
+<style >
+div {
+  width: 240px;
+  height: 240px;
+  background: #e9ecef;
+  color: #333;
+  padding: 20px;
+  box-shadow: 
+    7px 7px 12px rgba(0,0,0,.4),
+    -7px -7px 12px rgba(255,255,255, .9)
+  ;
+} 
+
+</style>
